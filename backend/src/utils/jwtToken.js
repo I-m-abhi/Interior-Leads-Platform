07bc8627f82becccd0cd077e605b04266dcbae9d
I=m-abhi <abhisasa2001@gmail.com>
2026-04-19 02:27:53 +0530

    update cookies options for production

--- backend/src/utils/jwtToken.js
+++ backend/src/utils/jwtToken.js
@@ -5,8 +5,8 @@ export const sendToken = (user, res, statusCode) => {
   const options = {
     expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    secure: false,
-    sameSite: "Lax",
+    secure: true,
+    sameSite: "None",
   }
 
   res.status(statusCode)
